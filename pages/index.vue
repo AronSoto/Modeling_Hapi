e43a8ea5502a162a4fcdb558003833d4aec31802
Aron <aronchino2g@hotmail.com>
2023-06-21 10:26:10 -0500

    maquetacionhapi

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hola</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+@import "assets/scss/variables"; 
+
+h1{
+    color: rgb(3, 219, 125);
+}
+
+
+</style>
